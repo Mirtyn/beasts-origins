365621705314fbe8ddc844a66a3d6b0e4b87b115
Mirtyn Kimpe <mirtyn@mirtyn.be>
2025-03-03 15:50:25 +0100

    initial push

---
+++ src/client/java/io/github/mirtyn/BeastsOriginsClient.java
@@ -0,0 +1,10 @@
+package io.github.mirtyn;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BeastsOriginsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
